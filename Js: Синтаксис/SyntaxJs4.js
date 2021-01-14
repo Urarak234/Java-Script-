545583706edcd0918abbc18a4011880fc3867fe7
Porcescu Dmitri <71259338+Urarak234@users.noreply.github.com>
2021-01-14 11:13:33 +0200

    Задание 1.4

---
+++ Js: Синтаксис/SyntaxJs4.js
@@ -0,0 +1,7 @@
+let ucFirst = function (str){
+
+alert(`Начальное слово:  ${str} 
+Слово с изменнёной первой буквой: ${str[0].toUpperCase() + str.slice(1)}`);
+}
+
+ucFirst(prompt('Введите слово, начинающееся с маленькой буквы '));
